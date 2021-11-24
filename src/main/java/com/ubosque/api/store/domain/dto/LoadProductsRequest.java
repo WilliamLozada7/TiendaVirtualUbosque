efc78bb401b6d86371624ba884822e1a28555e2a
WilliamLozada <william@cobre.co>
2021-11-23 20:25:11 -0500

    Eliminacion de parametro en request loadProduct

--- src/main/java/com/ubosque/api/store/domain/dto/LoadProductsRequest.java
+++ src/main/java/com/ubosque/api/store/domain/dto/LoadProductsRequest.java
@@ -1,7 +1,5 @@
 package com.ubosque.api.store.domain.dto;
 
-import java.util.Date;
-
 import javax.validation.constraints.NotNull;
 
 import lombok.Builder;
@@ -14,6 +12,4 @@ public class LoadProductsRequest {
 	@NotNull(message = "La URL del archivo CSV no puede ser null")
 	private String urlProducts;
 	
-	@NotNull(message = "La fecha no puede ser null")
-	private Date effectiveDate;
 }
